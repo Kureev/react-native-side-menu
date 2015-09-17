e1ad7789747b3844ba3921c9a29abab03a99a692
Kureev Alexey <kureev-mail@ya.ru>
2015-09-17 13:10:18 +0200

    Update examples

---
+++ examples/Basic/Menu.js
@@ -0,0 +1,61 @@
+const React = require('react-native');
+const Dimensions = require('Dimensions');
+const {
+  StyleSheet,
+  ScrollView,
+  View,
+  Image,
+  Text,
+  Component,
+} = React;
+
+const window = Dimensions.get('window');
+const uri = 'http://pickaface.net/includes/themes/clean/img/slide2.png';
+
+const styles = StyleSheet.create({
+  menu: {
+    flex: 1,
+    width: window.width,
+    height: window.height,
+    backgroundColor: 'gray',
+    padding: 20,
+  },
+  avatarContainer: {
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  avatar: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    flex: 1,
+  },
+  name: {
+    position: 'absolute',
+    left: 70,
+    top: 20,
+  },
+  item: {
+    fontSize: 14,
+    fontWeight: '300',
+    paddingTop: 5,
+  },
+});
+
+module.exports = class Menu extends Component {
+  render() {
+    return (
+      <ScrollView style={styles.menu}>
+        <View style={styles.avatarContainer}>
+          <Image
+            style={styles.avatar}
+            source={{ uri, }}/>
+          <Text style={styles.name}>Your name</Text>
+        </View>
+
+        <Text style={styles.item}>About</Text>
+        <Text style={styles.item}>Contacts</Text>
+      </ScrollView>
+    );
+  }
+}
