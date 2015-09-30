601cbe9d1333273fc829bf38475bee1f5487ab14
skv <skv-headless@yandex.ru>
2015-09-30 18:45:44 +0300

    fix orientation

--- styles.js
+++ styles.js
@@ -28,8 +28,6 @@ module.exports = StyleSheet.create({
     left: 0,
     top: 0,
     backgroundColor: 'transparent',
-    width: deviceScreen.width,
-    height: deviceScreen.height,
   },
   overlay: {
     position: 'absolute',
