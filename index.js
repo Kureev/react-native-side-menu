Kureev/react-native-side-menu

commit 601cbe9d1333273fc829bf38475bee1f5487ab14

fix orientation

index.js
@@ -228,9 +228,11 @@ class SideMenu extends Component {
       );
     }
 
+    const {width, height} = this.state;
+
     return (
       <Animated.View
-        style={[styles.frontView, this.props.animationStyle(this.state.left), ]}
+        style={[styles.frontView, {width, height}, this.props.animationStyle(this.state.left), ]}
         ref={(sideMenu) => this.sideMenu = sideMenu}
         {...this.responder.panHandlers}>
         {this.props.children}
@@ -252,13 +254,18 @@ class SideMenu extends Component {
     };
   }
 
+  onLayoutChange(e) {
+    const {width, height} = e.nativeEvent.layout;
+    this.setState({width, height});
+  }
+
   /**
    * Compose and render menu and content view
    * @return {React.Component}
    */
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.container} onLayout={this.onLayoutChange.bind(this)}>
         <View style={styles.menu}>
           {this.props.menu}
         </View>

styles.js
@@ -28,8 +28,6 @@ module.exports = StyleSheet.create({
     left: 0,
     top: 0,
     backgroundColor: 'transparent',
-    width: deviceScreen.width,
-    height: deviceScreen.height,
   },
   overlay: {
     position: 'absolute',

commit 013d734e880a6c7eb3f6735e7e5341f7ba584e35

Fix glitch with initial render
This is a regression introduced in
https://github.com/react-native-fellowship/react-native-side-menu/commit/601cbe9d1333273fc829bf38475bee1f5487ab14
because of two things:

1. onLayout is called (according to documentation), immediately once the layout has been calculated,
but the new layout may not yet be reflected on the screen at the time the event is received,
especially if a layout animation is in progress.

2. State is missing initial width & height properties

We have managed to workaround that issue by adding setTimeout so that
the points 1 & 2 were temporarily hidden.

The purpose of onLayout was to recalculate width/height on orientation
change, not on initialRender as well.

Thanks to these changes, the glitch with initialRender no longer exists.

There is also another nice side-improvement -> when quickly refreshing -
the main content was glitching anyway - because there was few ms
difference between initialRender and onLayout being called - at the
first sight content view was 0x0 px in size - that was causing all the
elements to be in the top left corner. Now it no longer exists.

index.js
@@ -44,7 +44,8 @@ class SideMenu extends Component {
     this.prevLeft = 0;
 
     this.state = {
-      shouldRenderMenu: false,
+      width: deviceScreen.width,
+      height: deviceScreen.height,
       left: new Animated.Value(0),
     };
   }
@@ -67,7 +68,7 @@ class SideMenu extends Component {
       onPanResponderRelease: this.handlePanResponderEnd.bind(this),
     });
   }
-  
+
   componentWillReceiveProps(props) {
     if (this.isOpen !== props.isOpen) {
       this.toggleMenu();
@@ -75,11 +76,6 @@ class SideMenu extends Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        shouldRenderMenu: true,
-      });
-    }, 500);
     if (this.props.defaultOpen) {
       this.openMenu();
     }
@@ -275,14 +271,7 @@ class SideMenu extends Component {
    * @return {React.Component}
    */
   render() {
-    let menu = null;
-
-    /**
-     * If menu is ready to be rendered
-     */
-    if (this.state.shouldRenderMenu) {
-      menu = <View style={styles.menu}>{this.props.menu}</View>;
-    }
+    const menu = <View style={styles.menu}>{this.props.menu}</View>;
 
     return (
       <View style={styles.container} onLayout={this.onLayoutChange.bind(this)}>